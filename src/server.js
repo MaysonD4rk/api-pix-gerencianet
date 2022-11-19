0f0daf589f6900d879dbd587ff1f293da9b3808b
MaysonD4rk <maysondamarante@hotmail.com>
2022-11-19 12:13:31 -0300

    removendo dotenv 2

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 
 if (process.env.NODE_ENV !== 'production') {
+    console.log('sa porra ta diferente')
     require('dotenv').config()
 }
+
 
 const express = require('express')
 const GNRequest = require('./apis/gerencianet.js')
