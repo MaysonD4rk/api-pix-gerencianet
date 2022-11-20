b524037bf7f89fd05203da2b721733d71cee36ed
MaysonD4rk <maysondamarante@hotmail.com>
2022-11-20 17:19:40 -0300

    adicionando api a wallet, parte final

--- src/server.js
+++ src/server.js
@@ -121,12 +121,72 @@ app.get('/charge/:userId', async (req, res)=>{
                     const date2 = new Date();
     
                     if ((date2.getTime() - date1.getTime()) / 1000 < 3000 && chargeDatas[i].status != "pago") {
+                        try {
+                            var deleteCob = await knex.delete().where({ chargeId: chargeDatas[i].chargeId }).table('charge');
+
+                            const endpoint = `${process.env.GN_ENDPOINT}/v2/cob`;
+
+                            const reqGN = await reqGNAlready;
+
+                            const dataCob = {
+                                calendario: {
+                                    expiracao: 3000
+                                },
+                                devedor: {
+                                    cpf: "12345678909",
+                                    nome: "Mswareg"
+                                },
+                                valor: {
+                                    original: `${valueToPay}.00`
+                                },
+                                chave: "db61e025-43f2-4b7d-82b6-58b4ee67959f",
+                                solicitacaoPagador: "Se possível, informe seu nickname para caso haja um improvável problema, devolver o seu dinheiro."
+                            }
+
+
+                            const cobResponse = await reqGN.post('v2/cob', dataCob)
+
+
+
+
+                            const qrcodeResponse = await reqGN.get(`v2/loc/${cobResponse.data.loc.id}/qrcode`)
+
+                            const saveDb = `{
+                        "calendario": { "criacao": "${cobResponse.data.calendario.criacao}", "expiracao": ${cobResponse.data.calendario.expiracao} },
+                        "txid": "${cobResponse.data.txid}",
+                        "revisao": ${cobResponse.data.revisao},
+                        "loc": {
+                            "id": ${cobResponse.data.loc.id},
+                            "location": "${cobResponse.data.loc.location}",
+                            "tipoCob": "${cobResponse.data.loc.tipoCob}",
+                            "criacao": "${cobResponse.data.loc.criacao}"
+                        },
+                        "location": "${cobResponse.data.location}",
+                        "status": "${cobResponse.data.status}",
+                        "devedor": { "cpf": "${cobResponse.data.devedor.cpf}", "nome": "${cobResponse.data.devedor.nome}" },
+                        "valor": { "original": "${cobResponse.data.valor.original}" },
+                        "chave": "${cobResponse.data.chave}",
+                        "solicitacaoPagador": "${cobResponse.data.solicitacaoPagador}",
+                        "userId": ${req.params.userId},
+                        "qrcode": "${qrcodeResponse.data.imagemQrcode}",
+                        "qrcodetxt": "${qrcodeResponse.data.qrcode}"
+                    }`
+
+                            try {
+                                const insertCharge = await knex.insert({ userId: req.params.userId, chargeId: cobResponse.data.txid, chargeValue: cobResponse.data.valor.original, chargeStatus: cobResponse.data.status, chargeJson: saveDb }).table("charge")
+                                console.log(insertCharge)
+                            } catch (error) {
+                                console.log(error)
+                            }
+                            
+                            res.json({
+                                imagem: qrcodeResponse.data.imagemQrcode,
+                                qrCodeTxt: qrcodeResponse.data.qrcode
+                            })
+                        } catch (error) {
+                            console.log(error)
+                        }
                         res.status(200)
-                        res.json({
-                            imagem: chargeDatas[i].chargeJson.qrcode,
-                            qrCodeTxt: chargeDatas[i].chargeJson.qrcodetxt,
-                            old: true
-                        })
 
                         break;
                     }else{
@@ -194,7 +254,67 @@ app.get('/charge/:userId', async (req, res)=>{
                             try {
                                 var deleteCob = await knex.delete().where({ chargeId: chargeDatas[i].chargeId }).table('charge')
                                 console.log(deleteCob);
-                                res.send('deletou');
+
+                                const endpoint = `${process.env.GN_ENDPOINT}/v2/cob`;
+
+                                const reqGN = await reqGNAlready;
+
+                                const dataCob = {
+                                    calendario: {
+                                        expiracao: 3000
+                                    },
+                                    devedor: {
+                                        cpf: "12345678909",
+                                        nome: "Mswareg"
+                                    },
+                                    valor: {
+                                        original: `${valueToPay}`
+                                    },
+                                    chave: "db61e025-43f2-4b7d-82b6-58b4ee67959f",
+                                    solicitacaoPagador: "Se possível, informe seu nickname para caso haja um improvável problema, devolver o seu dinheiro."
+                                }
+
+
+                                const cobResponse = await reqGN.post('v2/cob', dataCob)
+
+
+
+
+                                const qrcodeResponse = await reqGN.get(`v2/loc/${cobResponse.data.loc.id}/qrcode`)
+
+                                const saveDb = `{
+                        "calendario": { "criacao": "${cobResponse.data.calendario.criacao}", "expiracao": ${cobResponse.data.calendario.expiracao} },
+                        "txid": "${cobResponse.data.txid}",
+                        "revisao": ${cobResponse.data.revisao},
+                        "loc": {
+                            "id": ${cobResponse.data.loc.id},
+                            "location": "${cobResponse.data.loc.location}",
+                            "tipoCob": "${cobResponse.data.loc.tipoCob}",
+                            "criacao": "${cobResponse.data.loc.criacao}"
+                        },
+                        "location": "${cobResponse.data.location}",
+                        "status": "${cobResponse.data.status}",
+                        "devedor": { "cpf": "${cobResponse.data.devedor.cpf}", "nome": "${cobResponse.data.devedor.nome}" },
+                        "valor": { "original": "${cobResponse.data.valor.original}" },
+                        "chave": "${cobResponse.data.chave}",
+                        "solicitacaoPagador": "${cobResponse.data.solicitacaoPagador}",
+                        "userId": ${req.params.userId},
+                        "qrcode": "${qrcodeResponse.data.imagemQrcode}",
+                        "qrcodetxt": "${qrcodeResponse.data.qrcode}"
+                    }`
+
+                                try {
+                                    const insertCharge = await knex.insert({ userId: req.params.userId, chargeId: cobResponse.data.txid, chargeValue: cobResponse.data.valor.original, chargeStatus: cobResponse.data.status, chargeJson: saveDb }).table("charge")
+                                    console.log(insertCharge)
+                                } catch (error) {
+                                    console.log(error)
+                                }
+                                res.status(200)
+                                res.json({
+                                    imagem: qrcodeResponse.data.imagemQrcode,
+                                    qrCodeTxt: qrcodeResponse.data.qrcode
+                                })
+                                
                             } catch (error) {
                                 console.log(error)
                                 res.send('deu erro');
